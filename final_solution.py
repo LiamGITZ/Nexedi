afe5f304301d93cd8006f6231951147e3ba6fabc
liamgitz <lianwalsh@gmail.com>
2021-03-27 14:19:42 -0700

    updated comments and formmatting

--- final_solution.py
+++ final_solution.py
@@ -20,31 +20,26 @@ class Machine():
         return self.day_on_sale < machine2.day_on_sale
 
     def __repr__(self):
-        string = '(' \
-            +str(self.day_on_sale)+' '\
-            +str(self.price)+' '\
-            +str(self.resale)+' '\
-            +str(self.daily_earning)+' '\
-            +str(round(self.efficency,3))+' '\
-            +str(self.break_even_day)+' '\
-            +str(self.index)+' '\
-            +')'
+        string = string = (f'({self.day_on_sale} {self.price} {self.resale} {self.daily_earning}'
+                         + f' {round(self.efficiency, 3)} {self.break_even_day} {self.index})')
         return string
 
 @dataclass
 class Money():
     cash: int
-    resale_value: int
-    daily_earning: int
-    day_calculated: int
-    current_efficiency: float
+    resale_value: int = 0
+    daily_earning: int = 0
+    day_calculated: int = 0
+    current_efficiency: float = 0
 
 
 
 def find_max_profit(machines, starting_money, num_days):
-    current_money = Money(starting_money, 0, 0, 0, 0)
+    current_money = Money(starting_money)
     memo = [[] for i in range(len(machines))]
 
+    # the stack contains a list of tuples which contain the information needed to finish any given branch of the tree
+    # contain the remaining machines in the current branch of the tree and our current Money values
     stack = []
     stack.append((machines, current_money))
     max_profit = 0
@@ -52,16 +47,19 @@ def find_max_profit(machines, starting_money, num_days):
     while stack:
         machines, current_money = stack.pop(-1)
 
+        # reached leaf node in tree
         if len(machines) == 0:
-            total_capital = current_money.cash + current_money.resale_value + (current_money.daily_earning * (num_days - current_money.day_calculated))
+            total_capital = current_money.cash + current_money.resale_value + \
+                            (current_money.daily_earning * (num_days - current_money.day_calculated))
             if total_capital > max_profit:
                 max_profit = total_capital
             continue
 
-
+        #create a copy of machines so that we have our own list to edit in each instance
         machines = machines.copy()
         machine = machines.pop(0)
-        total_capital = current_money.cash + current_money.resale_value + (current_money.daily_earning * (machine.day_on_sale - current_money.day_calculated -1))
+        total_capital = current_money.cash + current_money.resale_value + \
+                        (current_money.daily_earning * (machine.day_on_sale - current_money.day_calculated -1))
 
         # searching our memo to see if we have been at this given level of the tree with strictly better circumstances
         if any(m[0] >= total_capital and m[1] >= current_money.current_efficiency
@@ -69,7 +67,7 @@ def find_max_profit(machines, starting_money, num_days):
             continue
 
         '''
-        appending not buys first means we process buys first
+        appending not-buys first means we process buys first
         this allows us to skip not buy scenarios where buying the machine is strictly better in terms of money and efficency
         '''
         if total_capital > machine.price:
@@ -102,10 +100,14 @@ while test_case != ['0','0','0']:
         machine = Machine(read_machine[0], read_machine[1], read_machine[2], read_machine[3], 0, num_days+1, i)
 
         if machine.day_on_sale != num_days:
-            machine.efficency =  (((num_days - machine.day_on_sale) * machine.daily_earning) + (machine.resale - machine.price)) / (num_days - machine.day_on_sale)
+            machine.efficency =  (((num_days - machine.day_on_sale) * machine.daily_earning) + 
+                                 (machine.resale - machine.price)) / (num_days - machine.day_on_sale)
+
             machine.break_even_day = machine.day_on_sale + math.ceil((machine.price - machine.resale) / machine.daily_earning)
+
             if machine.efficency > 0:
                 machines.append(machine)
+    # sorted first by day_on_sale, second by efficency, and finally by break_even_day
     machines = sorted(machines)
 
     money = find_max_profit(machines, current_money, num_days)
